12d289bb5de5aab27ed6fd088cbaaa40f1b1c23e
joseph-njogu <josephnjogu487@gmail.com>
2019-05-22 14:11:52 +0300

    these are the assignments given previously

---
+++ interns/read_csv.py
@@ -0,0 +1,12 @@
+import csv
+
+records = []
+with open('data.csv') as f:
+    reader = csv.reader(f) 
+    
+    for row in reader:
+        records.append(row[0])
+        
+    #for column in reader:
+       # records.append(column[1])
+print("{title}\n".format(title=records[1:]))
